67ae0f79a082930f1f61e311ff0f7890fda4a472
Leïth <essidleith@gmail.com>
2024-04-02 11:51:16 +0100

    inlining min css and html into JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,34 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var inlineSource = require('gulp-inline-source');
+var inlineCss = require('gulp-inline-css');
+var htmlmin = require('gulp-htmlmin');
+var minify = require('gulp-minify');
+var replace = require('gulp-replace');
+var fs = require('fs');
+var clean = require('gulp-clean');
 
-gulp.task('minify-js', function() {
-  return gulp.src('src/js/*.js')
-    .pipe(uglify())
-    .pipe(concat('widget.js'))
-    .pipe(gulp.dest('dist/js'));
+gulp.task('inline-css', function() {
+    return gulp.src('./src/html/widget.html')
+        .pipe(inlineCss({
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: true,
+            removeLinkTags: true
+        }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('src/css/*.css')
-    .pipe(cleanCSS())
-    .pipe(concat('widget.css'))
-    .pipe(gulp.dest('dist/css'));
+gulp.task('inject-html', function() {
+    var widgetHTML = fs.readFileSync('./dist/widget.html', 'utf8');
+    return gulp.src('./src/js/widget.js')
+        .pipe(replace('`', '`' + widgetHTML + '`'))
+        .pipe(minify())
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('inline-source', function() {
-  return gulp.src('dist/js/widget.js')
-    .pipe(inlineSource())
-    .pipe(gulp.dest('dist/js'));
+gulp.task('clean', function() {
+  return gulp.src(['dist/widget.html', 'dist/widget.js'], {read: false})
+      .pipe(clean());
 });
 
-gulp.task('default', gulp.series('minify-js', 'minify-css', 'inline-source'));
+gulp.task('default', gulp.series('inline-css', 'inject-html', 'clean'));
--- src/js/widget.js
+++ src/js/widget.js
@@ -6,26 +6,7 @@
 
   // Create the widget HTML
   var widgetHTML = `
-    <style inline src="../css/widget.css"></style>
-    <div id="askunali-widget" class="askunali-content">
-      <div class="askunali-content-wrapper">
-        <h1 class="askunali-title">ASK US <br>ANYTHING</h1>
-        <span class="askunali-subtitle">Ask about classes. Ask about nutrition, health and wellness. </span>
-
-        <div class="askunali-question-input-container">
-          <img src="magnifying_glass.svg" class="askunali-question-icon">
-          <div contenteditable="true" class="askunali-question-input-area" data-placeholder="Ask question here" id="askunali-question_input_div"></div>
-          <input type="hidden" id="askunali-question_input">
-        </div>
-        <button id="askunali-submit_button" class="askunali-submit-button">Ask ></button>
-
-        <div class="askunali-question-output-container">
-          <img src="rag-answer-icon.png" class="askunali-question-output-icon">
-          <div class="askunali-rag-answer-placeholder">View answer here.</div>
-          <div id="askunali-rag-answer"></div>
-        </div>
-      </div>
-    </div>
+    
   `;
 
   // Replace the target div with the widget HTML
