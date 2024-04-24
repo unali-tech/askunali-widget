c32987137cfe7d371bcb67a696222ad74de07d51
Leïth <essidleith@gmail.com>
2024-04-24 10:31:43 +0100

    97% done

--- src/js/widget.js
+++ src/js/widget.js
@@ -141,7 +141,10 @@
     function submitQuestion() {
       const question = editableDiv.textContent.trim();
       if (question !== '') {
-        fetch('https://unalihealth.com/ask_question', {
+        const outputIcon = document.getElementById('askunali-question-output-icon');
+        outputIcon.classList.add('loading');
+
+        fetch('https://possibly-rational-skink.ngrok-free.app/ask-question', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -153,6 +156,8 @@
         })
         .then(response => response.json())
         .then(data => {
+          outputIcon.classList.remove('loading');
+
           const answerBox = document.getElementById('askunali-answer');
           placeholder.style.display = 'none';
           answerBox.textContent = '';
@@ -190,6 +195,8 @@
             const separator = document.getElementById('askunali-separator');
             const sources = document.getElementById('askunali-sources');
             const link = document.getElementById('askunali-link');
+
+            appendShoppingContainer();
           
             setTimeout(() => {
               separator.style.display = 'block';
@@ -197,42 +204,124 @@
           
             setTimeout(() => {
               sources.style.display = 'flex';
-              updateSourcesLinks();
+              typeLink();
+              typeSourcesText();
             }, 1000);
+          }
+
+          function appendShoppingContainer() {
+            const shoppingContainer = document.getElementById('askunali-shopping-container');
+            shoppingContainer.style.display = 'flex';
           
-            setTimeout(() => {
-              link.style.display = 'block';
-            }, 1500);
+            const shoppingItems = data.shopping_data.activities;
+          
+            console.log(data.shopping_data);
+          
+            let shoppingIndex = 0;
+            const appendDelay = 200;
+          
+            function appendNextShoppingLink() {
+              if (shoppingIndex < shoppingItems.length) {
+                const item = shoppingItems[shoppingIndex];
+                const shoppingLink = document.createElement('a');
+                shoppingLink.classList.add('askunali-shopping-container-count');
+                shoppingLink.href = item.link;
+                shoppingLink.target = '_blank';
+                shoppingLink.textContent = item.display_name;
+                shoppingLink.style.cssText = `
+                  border: 1px solid #004695;
+                  color: #303840;
+                  border-radius: 20px;
+                  padding: 0px 15px;
+                  display: inline-flex;
+                  justify-content: center;
+                  align-items: center;
+                  margin-left: 8px;
+                  font-size: 16px;
+                  cursor: pointer;
+                `;
+                shoppingContainer.appendChild(shoppingLink);
+                shoppingIndex++;
+                setTimeout(appendNextShoppingLink, appendDelay);
+              }
+            }
+          
+            appendNextShoppingLink();
           }
           
-          function updateSourcesLinks() {
+          
+          function typeSourcesText() {
             const sourcesContainer = document.getElementById('askunali-sources');
             const totalSources = data.ingredients_data.length + data.activities_data.length;
-            
-            sourcesContainer.innerHTML = 'Sources: ';
           
-            [...data.ingredients_data, ...data.activities_data].forEach((item, index) => {
-              const sourceLink = document.createElement('a');
-              sourceLink.classList.add('askunali-sources-count');
-              sourceLink.href = item.paper_url;
-              sourceLink.target = '_blank';
-              sourceLink.textContent = index + 1;
-              sourceLink.style.cssText = `
-                border: 1px solid #3C444C;
-                color: #004695;
-                border-radius: 50%;
-                width: 22px;
-                height: 22px;
-                display: inline-flex;
-                justify-content: center;
-                align-items: center;
-                margin-left: 8px;
-                font-size: 14px;
-                cursor: pointer;
-              `;
-              sourcesContainer.appendChild(sourceLink);
-            });
+            let sourcesText = 'Sources: ';
+            let sourcesIndex = 0;
+            const typingSpeed = 50;
+          
+            function typeNextChar() {
+              if (sourcesIndex < sourcesText.length) {
+                sourcesContainer.innerHTML += sourcesText.charAt(sourcesIndex);
+                sourcesIndex++;
+                setTimeout(typeNextChar, typingSpeed);
+              } else {
+                appendSourceLinks();
+              }
+            }
+          
+            typeNextChar();
           }
+          
+          function appendSourceLinks() {
+            const sourcesContainer = document.getElementById('askunali-sources');
+            const sourceItems = [...data.ingredients_data, ...data.activities_data];
+            let sourceIndex = 0;
+            const appendDelay = 200;
+          
+            function appendNextSourceLink() {
+              if (sourceIndex < sourceItems.length) {
+                const item = sourceItems[sourceIndex];
+                const sourceLink = document.createElement('a');
+                sourceLink.classList.add('askunali-sources-count');
+                sourceLink.href = item.paper_url;
+                sourceLink.target = '_blank';
+                sourceLink.textContent = sourceIndex + 1;
+                sourceLink.style.cssText = `
+                  border: 1px solid #004695;
+                  color: #004695;
+                  border-radius: 50%;
+                  width: 22px;
+                  height: 22px;
+                  display: inline-flex;
+                  justify-content: center;
+                  align-items: center;
+                  margin-left: 8px;
+                  font-size: 14px;
+                  cursor: pointer;
+                `;
+                sourcesContainer.appendChild(sourceLink);
+                sourceIndex++;
+                setTimeout(appendNextSourceLink, appendDelay);
+              }
+            }
+          
+            appendNextSourceLink();
+          }
+          
+          function typeLink() {
+            const link = document.getElementById('askunali-link');
+            link.style.cssText = `
+              display: flex;
+              align-items: 
+              text-decoration: underline;
+              color: #303840;
+              font-size: 14px;
+            
+              position: absolute;
+              bottom: 0px;
+              right: 20px;
+            `;
+          }
+          
           
           function updateSourcesCount() {
             const sourcesCount = document.getElementById('askunali-sources-count');
@@ -268,6 +357,8 @@
         })
         .catch(error => {
           console.error('Error:', error);
+
+          outputIcon.classList.remove('loading');
         });
       }
     }    
